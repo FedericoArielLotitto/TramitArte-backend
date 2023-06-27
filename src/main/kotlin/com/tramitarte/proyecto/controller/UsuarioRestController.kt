dd7f50c08648085590c6d6626011f8aa3f62ad5d
MatePasto <mateo.pastorini@gmail.com>
2023-06-27 20:15:45 -0300

    Changes done

--- src/main/kotlin/com/tramitarte/proyecto/controller/UsuarioRestController.kt
+++ src/main/kotlin/com/tramitarte/proyecto/controller/UsuarioRestController.kt
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.stream.Collector
+import java.util.stream.Collectors
 
 @RestController
 @CrossOrigin("*")
@@ -22,6 +24,8 @@ class UsuarioRestController {
     }
 
     @GetMapping("/usuario/solicitante")
-    fun buscarSolicitantes(): List<Usuario> =
-        usuarioService.buscarPorRol(Rol.TRADUCTOR).map{ usuario -> usuario.nesecitaTraduccion == true }
+    fun buscarSolicitantes(): List<Usuario> {
+        var list = usuarioService.buscarPorRol(Rol.TRADUCTOR).stream()
+        return list.filter{ usuario -> usuario.nesecitaTraduccion }.collect(Collectors.toList())
+    }
 }
--- src/main/kotlin/com/tramitarte/proyecto/dominio/Usuario.kt
+++ src/main/kotlin/com/tramitarte/proyecto/dominio/Usuario.kt
@@ -7,12 +7,13 @@ import jakarta.persistence.Id
 
 
 @Entity
-class Usuario(nombre: String, apellido: String, rol: Rol) {
+class Usuario(nombre: String, apellido: String, rol: Rol, precio: Float) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
     var nombre: String = nombre
     var apellido: String = apellido
     var rol: Rol = rol
+    var precio: Float = precio
     var nesecitaTraduccion: Boolean = false
 }
